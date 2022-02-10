3c6e60c987a20cfc6d5e2ed61056b300aaf8d9b6
Johannes Edmeier <johannes.edmeier@steadybit.com>
2022-02-10 19:20:04 +0100

    test: fix wrong name of TestcontainerDnsResolverTest

--- src/test/java/com/steadybit/testcontainers/dns/TestcontainersDnsResolverTest.java
+++ src/test/java/com/steadybit/testcontainers/dns/TestcontainersDnsResolverTest.java
@@ -1,10 +1,10 @@
 package com.steadybit.testcontainers.dns;
 
+import com.steadybit.testcontainers.measure.EchoTcpContainer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -13,23 +13,21 @@ import java.util.Collections;
 import java.util.List;
 
 @Testcontainers
-public class TestcontainerDnsResolverTest {
+public class TestcontainersDnsResolverTest {
     @Container
-    static GenericContainer<?> toys = new GenericContainer<>("steadybit/bestseller-toys:1.0.0-SNAPSHOT")
-            .withExposedPorts(8081)
+    static GenericContainer<?> target = new EchoTcpContainer()
             .withExtraHost("some-extra-host", "192.168.2.1")
-            .withExtraHost("some-extra-host", "192.168.2.2")
-            .waitingFor(Wait.forHttp("/actuator/health"));
+            .withExtraHost("some-extra-host", "192.168.2.2");
 
     @Test
     void should_resolve() {
-        List<String> resolved = new TestcontainersDnsResolver(toys).resolve(Arrays.asList("127.0.0.1", "heise.de", "some-extra-host"));
+        List<String> resolved = new TestcontainersDnsResolver(target).resolve(Arrays.asList("127.0.0.1", "heise.de", "some-extra-host"));
         assertThat(resolved).containsExactlyInAnyOrder("127.0.0.1", "193.99.144.80", "192.168.2.1", "192.168.2.2");
     }
 
     @Test
     void should_throw_when_not_resolved() {
-        assertThatThrownBy(() -> new TestcontainersDnsResolver(toys).resolve(Collections.singletonList("doesnt-exist")))
+        assertThatThrownBy(() -> new TestcontainersDnsResolver(target).resolve(Collections.singletonList("doesnt-exist")))
                 .hasMessageContaining("doesnt-exist");
     }
 }
